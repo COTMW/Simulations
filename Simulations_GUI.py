71b99caae01ed1ef0cdcd3f287509db50a8876fa
COTMW <potyraoliwier@gmail.com>
2025-03-16 11:01:42 +0100

    Update Simulations_GUI.py

--- Simulations_GUI.py
+++ Simulations_GUI.py
@@ -87,45 +87,52 @@ monkey_frame.grid(column=0, row=1, sticky="nsew", padx=15, pady=15)
 monkey_frame.columnconfigure(0, weight=1)
 monkey_frame.rowconfigure(1, weight=1)
 
+# Birthday widgets
+bd_title = ttk.Label(birthday_frame, text="Birthday Paradox")
+bd_title.grid(row=0, column=0, columnspan=2)
+
 simulation_birthday_button = ttk.Button(birthday_frame, text="Simulation", command=birthday.simulation)
-simulation_birthday_button.grid(row=0, column=1, sticky="ew")
+simulation_birthday_button.grid(row=1, column=1, sticky="ew")
 
 sim_results = ttk.Label(birthday_frame, text="Chance: 0%")
-sim_results.grid(row=0, column=0, sticky="ew")
+sim_results.grid(row=1, column=0, sticky="ew")
 
 people = ttk.Label(birthday_frame, text=f"People: {birthday.num_people}")
-people.grid(row=1, column=0, sticky="ew")
+people.grid(row=2, column=0, sticky="ew")
 
 rolls = ttk.Label(birthday_frame, text=f"Rolls: {birthday.num_rolls}")
-rolls.grid(row=2, column=0, sticky="ew")
+rolls.grid(row=3, column=0, sticky="ew")
 
 set_people = ttk.Entry(birthday_frame, width=5)
-set_people.grid(row=1, column=1)
+set_people.grid(row=2, column=1)
 set_people.bind("<Return>", birthday.change_people)
 
 set_rolls = ttk.Entry(birthday_frame, width=5)
-set_rolls.grid(row=2, column=1)
+set_rolls.grid(row=3, column=1)
 set_rolls.bind("<Return>", birthday.change_rolls)
 
 february_checkbox = ttk.Checkbutton(birthday_frame, text="Include 29th February", variable=birthday.include_29_feb, command=birthday.include29feb)
-february_checkbox.grid(row=3, column=0, sticky="ew")
+february_checkbox.grid(row=4, column=0, sticky="ew")
 
 # Infinite Monkey Theorem widgets
+imt_title = ttk.Label(monkey_frame, text="Infinite Monkey Theorem")
+imt_title.grid(row=0, column=0, columnspan=2)
+
 monkey_simulation_button = ttk.Button(monkey_frame, text="Simulation", command=monkey.simulation)
-monkey_simulation_button.grid(row=0, column=1, sticky="ew")
+monkey_simulation_button.grid(row=1, column=1, sticky="ew")
 
 show_process_checkbox = ttk.Checkbutton(monkey_frame, text="Show process", variable=monkey.process)
-show_process_checkbox.grid(row=0, column=0, sticky="ew")
+show_process_checkbox.grid(row=1, column=0, sticky="ew")
 
 monkey_typing_history = tk.Listbox(monkey_frame)
-monkey_typing_history.grid(row=1, column=0, sticky="nsew", columnspan=2, rowspan=2)
+monkey_typing_history.grid(row=2, column=0, sticky="nsew", columnspan=2, rowspan=2)
 
 a_percentage = 100 / pow(len(monkey.alphabet), len(monkey.word))
-percentage_of_typing = tk.Label(text=f"{a_percentage}%")
-percentage_of_typing.grid(row=2, column=0, sticky="ew")
+percentage_of_typing = ttk.Label(text=f"{a_percentage}%")
+percentage_of_typing.grid(row=3, column=0, sticky="ew")
 
 set_new_word = tk.Entry()
-set_new_word.grid(row=3, column=0, sticky="ew")
+set_new_word.grid(row=4, column=0, sticky="ew")
 set_new_word.bind("<Return>", monkey.new_word)
 
 root.mainloop()
